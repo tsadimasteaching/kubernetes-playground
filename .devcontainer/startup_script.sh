b0011c173fe7ffc096270ef45de7cabc7b293f76
Anargyros Tsadimas <tsadimas@hua.gr>
2025-05-16 07:33:28 +0000

    added k9s

--- .devcontainer/startup_script.sh
+++ .devcontainer/startup_script.sh
@@ -1,20 +1,12 @@
 #!/usr/bin/bash
 echo "✅ Kubectl and Helm installed successfully"
 
-sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
-sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
-sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
-brew install derailed/k9s/k9s
+curl -sS https://webi.sh/k9s | sh
 
 echo "✅ kubectx, kubens, fzf, and k9s installed successfully"
 sudo apt-get install fzf -y
 
-alias k="kubectl"
-alias kga="kubectl get all"
-alias kgn="kubectl get all --all-namespaces"
-alias kdel="kubectl delete"
-alias kd="kubectl describe"
-alias kg="kubectl get"
+
 
 echo 'alias k="kubectl"' >> /home/$USER/.bashrc
 echo 'alias kga="kubectl get all"' >> /home/$USER/.bashrc
